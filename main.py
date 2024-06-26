4d03809f15b26ae4939b6a9642c5de9942772114
Vladimir <vladimirrybakov123@gmail.com>
2024-06-26 21:38:41 +0100

    drawing 4 separate circles of different colors and size so they give affect of overlapping

--- main.py
+++ main.py
@@ -9,6 +9,33 @@ WIDTH, HEIGHT = 600, 400
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Improver")
 
+class Target:
+    MAX_SIZE = 30
+    GROWTH_RATE = 0.2
+    COLOR = "red"
+    SECOND_COLOR = "white"
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.size = 0
+        self.grow = True
+
+    def update(self):
+        if self.size + self.GROWTH_RATE >= self.MAX_SIZE:
+            self.grow = False
+
+        if self.grow:
+            self.size += self.GROWTH_RATE
+        else:
+            self.size -= self.GROWTH_RATE
+        
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size)
+        pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
+        pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
+
 def main():
     run = True
     while run:
