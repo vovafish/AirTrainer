57130fa350797bcdacacbd45537acb7583777b15
Vladimir <vladimirrybakov123@gmail.com>
2024-06-26 21:05:28 +0100

    project init

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import random
+import math
+import time
+pygame.init()
+
+WIDTH, HEIGHT = 600, 400
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Aim Trainer")
+
