cf7baee5e323f21a19625573e53f4215b65388f9
Vladimir <vladimirrybakov123@gmail.com>
2024-06-26 21:09:00 +0100

    displays the init window

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ pygame.init()
 WIDTH, HEIGHT = 600, 400
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Aim Trainer")
+pygame.display.set_caption("Aim Improver")
 
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
