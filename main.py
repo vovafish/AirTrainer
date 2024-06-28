38960c9144ed164f621fb71dc61dd10f8b867501
Vladimir <vladimirrybakov123@gmail.com>
2024-06-28 20:27:15 +0100

    draw circles on the screena at 60 fps

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ WIDTH, HEIGHT = 600, 400
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Improver")
 
+TARGET_INCREMENT = 400
+TARGET_EVENT = pygame.USEREVENT
+
+TARGET_PADDING = 30
+
+BG_COLOR = (84, 69, 23)
+
 class Target:
     MAX_SIZE = 30
     GROWTH_RATE = 0.2
-    COLOR = "red"
-    SECOND_COLOR = "white"
+    COLOR = "yellow"
+    SECOND_COLOR = "blue"
 
     def __init__(self, x, y):
         self.x = x
@@ -36,14 +43,40 @@ class Target:
         pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
         pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
 
+
+def draw(win, targets):
+    win.fill(BG_COLOR)
+
+    for target in targets:
+        target.draw(win)
+    
+    pygame.display.update()
 def main():
     run = True
+    targets = []
+    clock = pygame.time.Clock()
+
+    pygame.time.set_timer(TARGET_EVENT, TARGET_INCREMENT)
+
     while run:
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+            if event.type == TARGET_EVENT:
+                x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
+                y = random.randint(TARGET_PADDING, HEIGHT - TARGET_PADDING)
+                target = Target(x, y)
+                targets.append(target)
     
+        for target in targets:
+            target.update()
+
+        draw(WIN, targets)
+
     pygame.quit()
 
 
